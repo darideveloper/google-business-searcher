08e2ce599bb874d8438b33592865908d8279f974
daridev <darideveloper@gmail.com>
2024-07-25 19:31:31 -0600

    feat (main): read bussiness data from excel

--- __main__.py
+++ __main__.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 
 from libs.scraper import Scraper
 from libs.csv_urils import CsvWriter
+from libs.xlsx import SpreadsheetManager
 
 # Env vaiables
 load_dotenv()
@@ -12,38 +13,47 @@ WAIT_TIME = int(os.getenv("WAIT_TIME"))
 
 # Paths
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
-EXCEL_FILE = os.path.join(CURRENT_DIR, "data.xlsx")
+EXCEL_FILE = os.path.join(CURRENT_DIR, "input.xlsx")
 OUTPUT_CSV = os.path.join(CURRENT_DIR, "output.csv")
 
 
 def main():
     
-    # TODO: Extract data from excel
-    bussinesses_data = [
-        ["DELMAR GARDENS OF MERAMEC VALLEY", "(636) 343-0016"],
-        ["CALHOUN NURSING & REHAB CENTER", "(618) 576-2278"],
-        ["GREENBRIER NURSING AND REHABILITATION CENTER", "(501) 679-0860"]
-    ]
+    print("Starting...")
     
-    # TODO: valdiate excel columns
-    
-    # Start scraper
+    # Start libs
     scraper = Scraper()
-    
-    # Start csv writer
     csv_writer = CsvWriter(OUTPUT_CSV)
+    ss_manager = SpreadsheetManager(EXCEL_FILE)
+    
+    # Read excel data
+    ss_manager.set_sheet("Sheet1")
+    bussinesses_data = ss_manager.get_data()
+    
+    # valdiate excel columns
+    header = ["Name", "Phone"]
+    header_sheet = bussinesses_data[0]
+    for header_element, header_sheet_element in zip(header, header_sheet):
+        if header_element != header_sheet_element:
+            print(f"Error: Column '{header_element}' not found in sheet 'Sheet1'")
+            print("Excel should have the following columns in the following order:")
+            print(" ".join(header))
+            quit()
     
     # Write csv header
     csv_writer.write_row(
         "w",
-        ["Business Name", "Phone", "Web Page", "Creation Date"]
+        ["Name", "Phone", "Web Page", "Creation Date"]
     )
 
     # Scrape each business
-    for row in bussinesses_data:
-        business_name, business_phone = row
+    for row in bussinesses_data[1:]:
+        business_name, business_phone, *_ = row
         
-        print(f"Searching '{business_name}' - {business_phone}...")
+        max_row_num = len(bussinesses_data) - 1
+        current_row_num = bussinesses_data.index(row)
+        counter = f"{current_row_num}/{max_row_num}"
+        print(f"({counter}) Searching '{business_name}' - {business_phone}...")
         
         web_page = scraper.get_web_page(business_name, business_phone)
         
